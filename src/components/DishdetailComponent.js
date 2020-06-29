e2e63a4a7201cafd9f10055e8aa1c2f0d6732c8a
itsmemohit <mohitdangwal016.md@gmail.com>
2020-06-30 00:44:42 +0530

    presentational and container component

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -58,9 +58,13 @@ class DishDetail extends Component {
          
         return(
             <div className="container">
-                <div className="col-12 col-md-5 m-1">
-                    {this.renderDish(this.props.dish)}   
-                    {this.renderComments(this.props.dish)}   
+                <div class="row">
+                    <div className="col-12 col-md-5 m-1">
+                        {this.renderDish(this.props.dish)}
+                    </div>     
+                    <div className="col-12 col-md-5 m-1">  
+                        {this.renderComments(this.props.dish)}   
+                    </div>
                 </div>
             </div>
             
